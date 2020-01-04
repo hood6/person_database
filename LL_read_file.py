a60e27a0cbace1a678791c310497e4485eb55a24
hood6 <47229201+hood6@users.noreply.github.com>
2020-01-04 14:24:35 -0500

    Changed LL_read_file.py
    Made some minor changes so that the file reader can read "professor" and "employee".

--- LL_read_file.py
+++ LL_read_file.py
@@ -3,29 +3,19 @@ from People.People import *
 
 if __name__ == "__main__":
     ll = SingleLinkedList()
-    ll.add_to_tail(Employee("test", 1, "test", "test"))
     file = open("test_file.txt", "r")
     for line in file:
         spl = line.split(":")
+        for i in spl:
+            i.rstrip()
         if spl[0].lower() == "person":
-            name = spl[1]
-            age = spl[2]
-            addr = spl[3]
-            ll.add_to_tail(Person(name, age, addr))
+            ll.add_to_tail(Person(spl[1], spl[2], spl[3]))
         if spl[0].lower() == "student":
             ll.add_to_tail(Student(spl[1], spl[2], spl[3], spl[4]))
-        if spl[0].lower == "professor":
-            name = spl[1]
-            age = spl[2]
-            addr = spl[3]
-            tenure = bool(spl[4])
-            ll.add_to_tail(Professor(name, age, addr, tenure))
-        if spl[0].lower == "employee":
-            name = spl[1]
-            age = spl[2]
-            addr = spl[3]
-            employer = spl[4]
-            ll.add_to_tail(Employee(name, age, addr, employer))
+        if spl[0].lower() == "professor":
+            ll.add_to_tail(Professor(spl[1], spl[2], spl[3], bool(spl[4])))
+        if spl[0].lower() == "employee":
+            ll.add_to_tail(Employee(spl[1], spl[2], spl[3], spl[4]))
         if spl[0] == "endline":
             file.close()
     ll.print_list()
