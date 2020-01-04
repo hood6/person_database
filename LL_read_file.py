e69f259b66f35c03984f1805a37ed9dbb8b4a19e
hood6 <47229201+hood6@users.noreply.github.com>
2020-01-04 14:38:24 -0500

    LL_read_file change
    Removed redundant new lines when adding to the linked list

--- LL_read_file.py
+++ LL_read_file.py
@@ -6,16 +6,14 @@ if __name__ == "__main__":
     file = open("test_file.txt", "r")
     for line in file:
         spl = line.split(":")
-        for i in spl:
-            i.rstrip()
         if spl[0].lower() == "person":
-            ll.add_to_tail(Person(spl[1], spl[2], spl[3]))
+            ll.add_to_tail(Person(spl[1], spl[2], spl[3].rstrip()))
         if spl[0].lower() == "student":
-            ll.add_to_tail(Student(spl[1], spl[2], spl[3], spl[4]))
+            ll.add_to_tail(Student(spl[1], spl[2], spl[3], spl[4].rstrip()))
         if spl[0].lower() == "professor":
-            ll.add_to_tail(Professor(spl[1], spl[2], spl[3], bool(spl[4])))
+            ll.add_to_tail(Professor(spl[1], spl[2], spl[3], bool(spl[4].rstrip())))
         if spl[0].lower() == "employee":
-            ll.add_to_tail(Employee(spl[1], spl[2], spl[3], spl[4]))
+            ll.add_to_tail(Employee(spl[1], spl[2], spl[3], spl[4].rstrip()))
         if spl[0] == "endline":
             file.close()
     ll.print_list()
